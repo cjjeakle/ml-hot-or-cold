eca32ee0419fcde9b35b7ab139aaf871ca1edc7b
Chris Jeakle <cjjeakle@umich.edu>
2020-02-02 01:58:46 -0600

    Basic website created

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # ML: Hot or Cold
 
 ## What is this?
-This is a machine learning model that classifies the sound of pouring water as hot or cold (given the arbitrary boundary that >100 degrees f is hot). This project is an attempt to use what I'm learning in Fast.ai's excellent MOOC, and was inspired by an [NPR piece](https://www.npr.org/2014/07/05/328842704/what-does-cold-sound-like) on the sound water makes when poured at different temperatures.
+This is a machine learning model that detects hot or cold water from the sound of pouring (given the arbitrary boundary that >100 degrees f is hot, and in practice the model saw a boundary of  >= 175 for hot and <= 75 for cold).
 
-[Build log 1](Build-Log-1_Design.ipynb) has a more detailed project description.
+This project is an effort to use what I'm learning in Fast.ai's excellent MOOC, and was inspired by an [NPR piece](https://www.npr.org/2014/07/05/328842704/what-does-cold-sound-like) on the sound water makes when poured at different temperatures.
+
+[Build log 1](Build-Log-1_Design.ipynb) starts with a more detailed project description.
 
 ## How to try the classifier:
-Submit an audio clip of pouring water here: *website forthcoming*
+Submit an audio clip of pouring water here: http://hot-or-cold.projects.chrisjeakle.com
 
 Alternatively: clone this repo, and follow the steps used in [build log 3](Build-Log-3_Testing-The-Model.ipynb) to test data of your own.
 
@@ -18,3 +20,19 @@ Take a look at the build logs to find out!
 [Build log 2](Build-Log-2_More-Data.ipynb) fine-tunes the chosen experimental design into a working model.
 
 Finally, [build log 3](Build-Log-3_Testing-The-Model.ipynb) tries out the model on some data the model has never seen before (not even for validation).
+
+There is a simple infrence pipeline defined in [server.py](website/server.py) under the `evaluate_audio_sample` route.
+
+## Running the website
+
+1. Install dependencies:
+    * `pip3 install starlette uvicorn aiofiles python-multipart fastai torch librosa`
+1. Start the server:
+    * `python3 website/server.py`
+1. Navigate to: http://localhost:8001
+
+## Hosting the website
+
+1. Follow the steps [here](https://www.nginx.com/blog/using-free-ssltls-certificates-from-lets-encrypt-with-nginx/) to set up nginx as a reverse proxy (to provide https support)
+    * Point the reverse proxy to a running instance of `website/server.py` at localhost:8001
+1. Start the python webserver `python3 website/server.py`
---
+++ website/server.py
@@ -0,0 +1,82 @@
+# Web server libs
+from starlette.applications import Starlette
+from starlette.templating import Jinja2Templates
+
+# Libs for spectrogram generation
+import os
+import io
+import soundfile
+import librosa
+import librosa.display
+import matplotlib.pyplot
+
+# Libs for inference
+from fastai.vision import *
+
+# Other libs
+from pathlib import Path
+import uvicorn
+
+# Set up the server
+app = Starlette()
+templates = Jinja2Templates(directory="templates")
+
+# Set up the model for infrence
+defaults.device = torch.device("cpu")
+modelPath = Path("../data-v2/spectrograms/")
+learn = load_learner(modelPath)
+
+def generate_tempfile_libROSA_spectrogram(audio_file):
+    min_frequency = 256
+    max_frequency = 16384
+    samples_between_frames = 256
+    fft_window_size = 2048
+
+    # Load the audio, generate a spectrogram in DB (to match the logarithmic nature of human hearing)
+    audio_data, sample_rate = librosa.load(audio_file, sr=None)
+    spectrogram = librosa.feature.melspectrogram(y=audio_data, sr=sample_rate, n_mels=128, fmax=max_frequency,
+                                             hop_length=samples_between_frames, n_fft=fft_window_size)
+    amplitude_in_db = librosa.power_to_db(spectrogram, ref=np.max)
+
+    # Generate the spectrogram with min and max frequency bounds applied
+    librosa.display.specshow(amplitude_in_db, x_axis=None, y_axis=None, sr=sample_rate, fmin=min_frequency, fmax=max_frequency)
+    plt.tight_layout()
+
+    # Save the generated spectrogram
+    outputFile = tempfile.TemporaryFile(suffix="png")
+    plt.savefig(outputFile)
+    plt.close()
+
+    return outputFile
+
+def predict_hot_or_cold(audio_buffer):
+    with generate_tempfile_libROSA_spectrogram(audio_buffer) as tempSpectrogram:
+        spectrogram = open_image(tempSpectrogram)
+        return learn.predict(spectrogram)
+
+@app.route("/evaluate_audio_sample", methods=["POST"])
+async def evaluate_audio_sample(request):
+    formData = await request.form()
+    audio_bytes = await formData["file"].read()
+    audio_file_ext = formData["file"].filename[-4:];
+    with tempfile.NamedTemporaryFile(suffix=audio_file_ext) as audio_file:
+        audio_file.write(audio_bytes)
+        pred_classification, pred_idx, outputs = predict_hot_or_cold(audio_file.name)
+        return templates.TemplateResponse("audio-analysis-result.html", {
+            "request": request,
+            "prediction": str(pred_classification),
+            "probability_cold" : outputs[0].item(),
+            "probability_hot" : outputs[1].item(),
+        })
+
+@app.route("/how-to")
+def how_to(request):
+    return templates.TemplateResponse("how-to.html", {"request": request})
+
+
+@app.route("/")
+def index(request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8001)
